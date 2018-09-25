626dcc2e82fb44452cb96c17e1797236693b780c
kimhyeri <hyer1k@naver.com>
2018-09-25 18:57:18 +0900

    Create log model

---
+++ ManageSponsor/Log/Model/LogModel.swift
@@ -0,0 +1,19 @@
+//
+//  LogModel.swift
+//  ManageSponsor
+//
+//  Created by hyerikim on 2018. 9. 25..
+//  Copyright © 2018년 hyerikim. All rights reserved.
+//
+
+import UIKit
+
+class LogModel {
+    var log : String
+    var date : String
+    
+    init(log: String, date: String) {
+        self.log = log
+        self.date = date
+    }
+}
